89c5345709df0b3195eebc63ad42038aa865a5b3
SheeperGit <sean1900.s@gmail.com>
2023-03-07 10:35:57 -0500

    Ex8 and A2 Distro Pushed

---
+++ Exercises_Directory/catching_a_break_1008305371.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("* My favorite thing about A48 so far is:\n");
+    printf("Paco's lectures!\n");
+    printf("I think Paco is a phenomenal instructor, but I feel that his exams are too tricky...\n\n");
+
+    printf("* My least favorite thing about A48 so far:\n");
+    printf("The overabundance of MARA.\n");
+    printf("I think it's best to say whether or not a problem has all the information you need to solve it or if it's up to our design.\n\n");
+
+    printf("* What am I going to do to improve during the last part of the course:\n");
+    printf("Do a LOT more practice exercises. I need to strengthen my problem-solving.\n");
+    printf("I also want to learn to use my debugger more, as well as Git and Github to more efficiently solve problems.\n");
+
+    return 0;
+}
