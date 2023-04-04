6265c0f41a38b081a94b723f12c2831f55016b30
SheeperGit <sean1900.s@gmail.com>
2023-04-04 10:43:00 -0400

    Ex12

---
+++ Exercises_Directory/summer_plans_1008305371.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Over the summer, I plan to...\n\n");
+
+    printf("Take three CS summer courses to catch up to the guys who made POSt.\n");
+    printf("Along with a math course, just for fun!\n");
+    printf("Learn how to bake/cook.\n");
+    printf("Play video games with my friends!\n");
+    printf("Read some books... Man, that'll be relaxing....\n");
+
+    return 0;
+}
